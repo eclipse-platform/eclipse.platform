8829488b59df5f3198f48c05baac9c25ff870282
Christopher Elek <celek>
2002-10-08 11:20:50 +0000

    remove originating sites from core

--- update/org.eclipse.update.core/src/org/eclipse/update/configuration/ILocalSite.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/configuration/ILocalSite.java
@@ -261,20 +261,4 @@ public interface ILocalSite extends IAdaptable {
 	 */
 	public IStatus getFeatureStatus(IFeature feature) throws CoreException ;	
 
-	/**
-	 * returns the <code>URL</code> the feature was installed from
-	 * or <code>null</code>
-	 * 
-	 * @param feature the feature
-	 * @return the URL of the originiting site of the feature
-	 * @exception CoreException
-	 * @since 2.0.2
-	 * <p>
-	 * <b>Note:</b> This method is part of an interim API that is still under development and expected to
-	 * change significantly before reaching stability. It is being made available at this early stage to solicit feedback
-	 * from pioneering adopters on the understanding that any code that uses this API will almost certainly be broken
-	 * (repeatedly) as the API evolves.
-	 * </p>
-	 */
-	public URL getOriginatingSiteURL(IFeature feature) throws CoreException;
 }
--- update/org.eclipse.update.core/src/org/eclipse/update/internal/core/SiteLocal.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/internal/core/SiteLocal.java
@@ -37,9 +37,6 @@ public class SiteLocal extends SiteLocalModel implements ILocalSite, IWritable {
 	 */
 	public static boolean newFeaturesFound = false;
 
-	// the map of originating sites
-	private static OriginatingSitesProperties originatingSites = null;
-
 	/*
 	 * initialize the configurations from the persistent model.
 	 * Set the reconciliation as non optimistic
@@ -215,9 +212,6 @@ public class SiteLocal extends SiteLocalModel implements ILocalSite, IWritable {
 		// and set runtim info for next startup
 		 ((InstallConfiguration) getCurrentConfiguration()).save(isTransient());
 		 
-		// save the originating sites
-		OriginatingSitesProperties.getDefault().store();
-
 		// save the local site
 		if ("file".equalsIgnoreCase(getLocationURL().getProtocol())) { //$NON-NLS-1$
 			File file = null;
@@ -771,31 +765,4 @@ public class SiteLocal extends SiteLocalModel implements ILocalSite, IWritable {
 		return getSiteStatusAnalyzer().getFeatureStatus(feature);
 	}
 
-	/**
-	 * Method getOriginatingSite.
-	 * @param feature
-	 * @return URL
-	 */
-	public URL getOriginatingSiteURL(IFeature feature) throws CoreException {
-		if (originatingSites == null)
-			originatingSites = OriginatingSitesProperties.getDefault();
-		if (feature == null) {
-			throw Utilities.newCoreException(Policy.bind("SiteLocal.NullFeature"), null);
-		}
-
-		// retrieve the originatingSite
-		String featureID = feature.getURL().toExternalForm();
-		String siteString = originatingSites.getProperty(featureID);
-		if (siteString == null)
-			return null;
-
-		// transform in URL
-		URL siteURL;
-		try {
-			siteURL = new URL(siteString);
-		} catch (MalformedURLException e) {
-			throw Utilities.newCoreException(Policy.bind("SiteLocal.WrongURL"), null);
-		}
-		return siteURL;
-	}
 }
