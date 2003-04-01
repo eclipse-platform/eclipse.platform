aeedd8ce797dfed6e36aa33851dfe227aa004265
Christopher Elek <celek>
2003-04-01 18:19:56 +0000

    2.0.3.1

--- update/org.eclipse.update.tests.core/src/org/eclipse/update/tests/regularInstall/TestLocalSite.java
+++ update/org.eclipse.update.tests.core/src/org/eclipse/update/tests/regularInstall/TestLocalSite.java
@@ -203,7 +203,8 @@ public class TestLocalSite extends UpdateManagerTestCase {
 		// there are 2 configuration
 		String time = ""+site.getCurrentConfiguration().getCreationDate().getTime();
 		URL location = ((SiteLocal)site).getLocationURL();		
-		File file = new File(new URL(location,"Config"+time+".xml").getFile());
+		//File file = new File(new URL(location,"Config"+time+".xml").getFile());
+		File file = new File(new URL(location,SiteLocal.DEFAULT_CONFIG_PREFIX+time+".xml").getFile());		
 		assertTrue("new configuration does not exist", file.exists());
 		
 		// teh current one points to a real fature
@@ -286,7 +287,8 @@ public class TestLocalSite extends UpdateManagerTestCase {
 		// there are 2 configuration
 		String time = ""+site.getCurrentConfiguration().getCreationDate().getTime();
 		URL location = ((SiteLocal)site).getLocationURL();		
-		File file = new File(new URL(location,"Config"+time+".xml").getFile());
+		//File file = new File(new URL(location,"Config"+time+".xml").getFile());
+		File file = new File(new URL(location,SiteLocal.DEFAULT_CONFIG_PREFIX+time+".xml").getFile());		
 		assertTrue("new configuration does not exist", file.exists());
 		
 		// teh current one points to a real fature
