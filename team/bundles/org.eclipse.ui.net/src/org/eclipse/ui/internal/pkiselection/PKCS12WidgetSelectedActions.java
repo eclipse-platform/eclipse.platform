aa209785437e45d9ba95dc2421cd5c44cb537362
jjschia <jjschia@evoforge.org>
2023-10-11 13:26:33 +0000

    Initial Commit

---
+++ team/bundles/org.eclipse.ui.net/src/org/eclipse/ui/internal/pkiselection/PKCS12WidgetSelectedActions.java
@@ -0,0 +1,24 @@
+package org.eclipse.ui.internal.pkiselection;
+
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.internal.util.KeyStoreFormat;
+
+public class PKCS12WidgetSelectedActions {
+
+	public static void setKeyStoreFormat(){
+		PKCSSelected.setPkcs12Selected(true);
+		PKCSSelected.setPkcs11Selected(false);
+		PKCSSelected.setKeystoreformat(KeyStoreFormat.PKCS12);
+	}
+
+	public static void userInterfaceDisplay(Text certpathtext, Label pkiLabel,
+			Label passwordlabel, Text passwordtext, Button browseButton, boolean visiblity) {
+		certpathtext.setVisible(visiblity);
+		pkiLabel.setVisible(visiblity);
+		browseButton.setVisible(visiblity);
+		passwordlabel.setVisible(visiblity);
+		passwordtext.setVisible(visiblity);
+	}
+}
