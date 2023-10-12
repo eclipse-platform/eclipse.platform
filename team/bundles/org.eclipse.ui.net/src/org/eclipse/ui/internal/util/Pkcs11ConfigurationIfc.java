b866372daa98badd093e3dc983c3d66402cf65e4
jjschia <jjschia@evoforge.org>
2023-10-12 11:19:25 +0000

    Initial Commit

---
+++ team/bundles/org.eclipse.ui.net/src/org/eclipse/ui/internal/util/Pkcs11ConfigurationIfc.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ui.internal.util;
+
+public interface Pkcs11ConfigurationIfc {
+
+	public String getImplementationName();
+
+	public String getConfigurationFilename();
+
+	public String getConfigurationLocationDir();
+
+	public String getLibraryFilename();
+
+	public String getLibraryLocationDir();
+
+}
