64d7f2f34af9423f0444afe08f553e9346fe0757
jjschia <jjschia@evoforge.org>
2023-10-19 11:10:08 +0000

    Initial Commit

---
+++ team/bundles/org.eclipse.ui.net/src/org/eclipse/ui/internal/util/SpecialClassLoader.java
@@ -0,0 +1,37 @@
+package org.eclipse.ui.internal.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class SpecialClassLoader extends ClassLoader {
+
+	private String findThisClassAt = null;
+
+	public SpecialClassLoader(String classlocation){
+		findThisClassAt = classlocation;
+	}
+
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException{
+		try{
+			byte[] classBytes = null;
+			classBytes = loadClassbytes(name);
+			Class<?> cl = defineClass(name, classBytes, 0, classBytes.length);
+			if (cl == null) throw new ClassNotFoundException(name);
+			return cl;
+		} catch (IOException e){
+			throw new ClassNotFoundException(name);
+		}
+	}
+
+	private byte[] loadClassbytes(String name) throws IOException {
+
+		return Files.readAllBytes(Paths.get(findThisClassAt));
+	}
+
+	public String getFindThisClassAtLocation() {
+		return findThisClassAt;
+	}
+
+}
