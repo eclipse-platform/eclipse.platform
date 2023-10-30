72f56715452b705013e46a3c3b488a6652b6c59a
jjschia <jjschia@evoforge.org>
2023-10-30 06:59:41 +0000

    Initial Commit

---
+++ team/bundles/org.eclipse.core.pki/src/org/eclipse/pki/auth/PasswordCallbackHandler.java
@@ -0,0 +1,16 @@
+package org.eclipse.pki.auth;
+
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+public class PasswordCallbackHandler implements CallbackHandler {
+
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
