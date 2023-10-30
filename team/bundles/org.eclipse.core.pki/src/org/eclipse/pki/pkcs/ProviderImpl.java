e808732246f165b561c980653f5907b447508e0d
Java Joe <joe@schiavone.org>
2023-10-30 10:06:34 +0000

    Initial Commit

---
+++ team/bundles/org.eclipse.core.pki/src/org/eclipse/pki/pkcs/ProviderImpl.java
@@ -0,0 +1,26 @@
+package org.eclipse.pki.pkcs;
+
+import java.util.List;
+
+import javax.net.ssl.SSLContext;
+
+import org.eclipse.pki.interfaces.IProvider;
+
+public class ProviderImpl implements IProvider  {
+	public static SSLPkcs11Provider security = SSLPkcs11Provider.getInstance();
+	public static SSLPkcs11Provider getNewInstance() {
+		security=SSLPkcs11Provider.getInstance();
+		return security;
+	}
+	@SuppressWarnings("unchecked")
+	public List<String> getList() {
+		return (List<String>) security.getList();
+	}
+	public String getAlias() {
+		return security.getAlias();
+	}
+	@SuppressWarnings("static-access")
+	public void off() {
+		security.disable();
+	}
+}
