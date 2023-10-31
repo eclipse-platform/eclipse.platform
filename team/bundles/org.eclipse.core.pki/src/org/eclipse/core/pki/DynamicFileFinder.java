5020172d91475e880003e4e9ae6e0ea6a68f3a7b
jjschia <jjschia@evoforge.org>
2023-10-31 09:53:26 +0000

    Customize

--- team/bundles/org.eclipse.core.pki/src/org/eclipse/core/pki/DynamicFileFinder.java
+++ team/bundles/org.eclipse.core.pki/src/org/eclipse/core/pki/DynamicFileFinder.java
@@ -9,7 +9,6 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 
-import secure.eclipse.authentication.provider.debug.DebugLogger;
 
 public class DynamicFileFinder extends SimpleFileVisitor <Path>  {
 	private boolean found=false;
