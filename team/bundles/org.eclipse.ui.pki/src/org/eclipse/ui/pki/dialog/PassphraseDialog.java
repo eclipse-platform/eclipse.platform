a6fb3e4c5b123feb6633c9292b5b1bf45173bbfe
jjschia <jjschia@evoforge.org>
2023-12-06 11:07:52 +0000

    Set project location

---
+++ team/bundles/org.eclipse.ui.pki/src/org/eclipse/ui/pki/dialog/PassphraseDialog.java
@@ -0,0 +1,90 @@
+package org.eclipse.ui.pki.dialog;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+
+public class PassphraseDialog extends Dialog{
+  protected Text passphraseField;
+  protected String passphrase=null;
+  protected String message=null;
+
+  public PassphraseDialog(Shell parentShell, String message){
+    super(parentShell);
+    this.message=message;
+  }
+
+  protected void configureShell(Shell newShell){
+    super.configureShell(newShell);
+    newShell.setText(message);
+  }
+
+  public void create(){
+    super.create();
+    passphraseField.setFocus();
+  }
+
+  protected Control createDialogArea(Composite parent){
+    initializeDialogUnits(parent);
+    Composite main=new Composite(parent, SWT.NONE);
+
+    GridLayout layout=new GridLayout();
+    layout.numColumns=3;
+    layout.marginHeight=convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_MARGIN);
+    layout.marginWidth=convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_MARGIN);
+    layout.verticalSpacing=convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_SPACING);
+    layout.horizontalSpacing=convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
+    main.setLayout(layout);
+    main.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+    if(message!=null){
+      Label messageLabel=new Label(main, SWT.WRAP);
+      messageLabel.setText(message);
+      GridData data=new GridData(GridData.FILL_HORIZONTAL);
+      data.horizontalSpan=3;
+      messageLabel.setLayoutData(data);
+    }
+
+    createPassphraseFields(main);
+    Dialog.applyDialogFont(main);
+    return main;
+  }
+
+  protected void createPassphraseFields(Composite parent){
+    new Label(parent, SWT.NONE).setText("Password");
+    passphraseField=new Text(parent, SWT.BORDER);
+    GridData data=new GridData(GridData.FILL_HORIZONTAL);
+    data.widthHint=convertHorizontalDLUsToPixels(IDialogConstants.ENTRY_FIELD_WIDTH);
+    passphraseField.setLayoutData(data);
+    passphraseField.setEchoChar('*');
+
+    new Label(parent, SWT.NONE);
+  }
+
+  public String getPassphrase(){
+    return passphrase;
+  }
+
+  protected void okPressed(){
+    String _passphrase=passphraseField.getText();
+    if(_passphrase==null||_passphrase.length()==0){
+      return;
+    }
+    passphrase=_passphrase;
+    super.okPressed();
+  }
+
+  protected void cancelPressed(){
+    passphrase=null;
+    super.cancelPressed();
+  }
+}
+
---
+++ team/bundles/org.eclipse.ui.pki/src/org/eclipse/ui/pki/preferences/TrustStoreLocationFieldEditor.java
@@ -0,0 +1,71 @@
+package org.eclipse.ui.pki.preferences;
+
+import java.security.KeyStore;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.StringButtonFieldEditor;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.pki.auth.AuthenticationPlugin;
+import org.eclipse.pki.exception.UserCanceledException;
+import org.eclipse.pki.util.ChangedPressedFieldEditorStatus;
+
+/**
+ * This class can be used in an {@link FieldEditorPreferencePage} to represent a trust store
+ * location. Validation is performed as the user enters a new value.
+ * @since 1.3
+ *
+ */
+public class TrustStoreLocationFieldEditor extends StringButtonFieldEditor {
+
+	public TrustStoreLocationFieldEditor(String name, String labelText,
+			Composite parent) {		
+		init(name, labelText);
+		setEmptyStringAllowed(false);
+        setErrorMessage(JFaceResources
+                .getString("DirectoryFieldEditor.errorMessage"));//$NON-NLS-1$
+        setChangeButtonText(JFaceResources.getString("openChange"/*"openBrowse"*/));//$NON-NLS-1$
+        setValidateStrategy(VALIDATE_ON_FOCUS_LOST);        
+        createControl(parent);
+	}
+
+
+	@Override
+	protected String changePressed(){
+		ChangedPressedFieldEditorStatus.setJksChangedPressed(true);
+        String fileName;
+        KeyStore existingJKSTrustStore = null;
+    	try{
+    		
+    		existingJKSTrustStore = AuthenticationPlugin.getDefault().getExistingTrustStore();
+    		    		
+    		ChangedPressedFieldEditorStatus.setJksTrustStore(existingJKSTrustStore);
+    		
+    		if(AuthenticationPlugin.getDefault().getExistingTrustStore() != null){
+    			AuthenticationPlugin.getDefault().setTrustStore(null);
+    		}
+    		
+        	AuthenticationPlugin.getDefault().getJKSTrustStore(); 
+        	
+    	}  catch (UserCanceledException e) {
+    		
+    		ChangedPressedFieldEditorStatus.setJksChangedPressed(false);
+    		ChangedPressedFieldEditorStatus.setJKSSaveTrustStoreChecked(false);
+    		ChangedPressedFieldEditorStatus.setJksTrustStore(null);
+    		
+    		AuthenticationPlugin.getDefault().setTrustStore(existingJKSTrustStore);
+    		AuthenticationPlugin.getDefault().getPreferenceStore().setValue(
+					AuthenticationPreferences.TRUST_STORE_LOCATION, AuthenticationPlugin.getDefault().obtainSystemPropertyJKSPath());
+    		AuthenticationPlugin.getDefault().setTrustStorePassPhrase(AuthenticationPlugin.getDefault().obtainSystemPropertyJKSPass());
+    		
+			//System.out.println("Unable to set javax.net.ssl properties!");
+		}        
+        
+        fileName = AuthenticationPlugin.getDefault().getPreferenceStore().getString(AuthenticationPreferences.TRUST_STORE_LOCATION);
+        
+        if ( null != fileName ) {
+        	return fileName;
+        }
+		        
+        return null;
+	}
+}
