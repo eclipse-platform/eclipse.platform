013684e956b0ff90c5266ec26a74fc2cb05d7f59
jjschia <jjschia@evoforge.org>
2023-12-11 09:28:12 +0000

    Add new classes

---
+++ team/bundles/org.eclipse.core.pki/src/org/eclipse/pki/auth/PKIState.java
@@ -0,0 +1,20 @@
+package org.eclipse.pki.auth;
+
+public enum PKIState {
+	CONTROL;
+	private boolean isPKCS11on=false;
+	private boolean isPKCS12on=false;
+	public boolean isPKCS11on() {
+		return isPKCS11on;
+	}
+	public void setPKCS11on(boolean isPKCS11on) {
+		this.isPKCS11on = isPKCS11on;
+	}
+	public boolean isPKCS12on() {
+		return isPKCS12on;
+	}
+	public void setPKCS12on(boolean isPKCS12on) {
+		this.isPKCS12on = isPKCS12on;
+	}
+
+}
