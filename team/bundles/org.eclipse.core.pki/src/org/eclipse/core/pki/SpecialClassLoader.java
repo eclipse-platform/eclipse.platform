758f0629f07b3e4245879205b3adbf65b1890513
joe <joe@schiavone.org>
2023-12-21 05:42:20 -0500

    add lic

--- team/bundles/org.eclipse.core.pki/src/org/eclipse/core/pki/SpecialClassLoader.java
+++ team/bundles/org.eclipse.core.pki/src/org/eclipse/core/pki/SpecialClassLoader.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Eclipse Platform, Security Group and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eclipse Platform - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.core.pki;
 
 import java.io.IOException;
@@ -5,13 +18,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class SpecialClassLoader extends ClassLoader {
-	
+
 	private String findThisClassAt = null;
-		
+
 	public SpecialClassLoader(String classlocation){
 		findThisClassAt = classlocation;
 	}
-	
+
+	@Override
 	protected Class<?> findClass(String name) throws ClassNotFoundException{
 		try{
 			byte[] classBytes = null;
@@ -25,7 +39,7 @@ public class SpecialClassLoader extends ClassLoader {
 	}
 
 	private byte[] loadClassbytes(String name) throws IOException {
-		
+
 		return Files.readAllBytes(Paths.get(findThisClassAt));
 	}
 
