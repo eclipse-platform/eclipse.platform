675dd643a5e151efa968b892fb143664a217211e
joe <joe@schiavone.org>
2023-12-24 12:30:04 -0500

    add lic

--- team/bundles/org.eclipse.ui.pki/src/org/eclipse/ui/pki/pkiselection/PKCS12WidgetSelectedActions.java
+++ team/bundles/org.eclipse.ui.pki/src/org/eclipse/ui/pki/pkiselection/PKCS12WidgetSelectedActions.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Eclipse Platform, Security Group and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eclipse Platform - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ui.pki.pkiselection;
 
 import org.eclipse.swt.widgets.Button;
