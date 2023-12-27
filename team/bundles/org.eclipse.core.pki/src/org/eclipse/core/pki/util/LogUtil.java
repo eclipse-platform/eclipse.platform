518780ba741b058395950ceef7c544d64962fb1f
jjschia <jjschia@evoforge.org>
2023-12-27 09:47:44 +0000

    Add Generics

--- team/bundles/org.eclipse.core.pki/src/org/eclipse/core/pki/util/LogUtil.java
+++ team/bundles/org.eclipse.core.pki/src/org/eclipse/core/pki/util/LogUtil.java
@@ -28,28 +28,28 @@ public class LogUtil {
 	public static void logInfo(String message) {
 		StackWalker stackWalker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 		final var pluginName = stackWalker.getCallerClass();
-		final ServiceCaller<ILog> log = new ServiceCaller(pluginName, ILog.class);
+		final ServiceCaller<ILog> log = new ServiceCaller<>(pluginName, ILog.class);
 		log.call(logger -> logger.info(message));
 	}
 	public static void logError(String message, Throwable t) {
 		StackWalker stackWalker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 		final var pluginName = stackWalker.getCallerClass();
-		final ServiceCaller<ILog> log = new ServiceCaller(pluginName, ILog.class);
+		final ServiceCaller<ILog> log = new ServiceCaller<>(pluginName, ILog.class);
 		log.call(logger -> logger.error(message));
 	}
 
 	public static void logWarning(String message) {
 		StackWalker stackWalker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 		final var pluginName = stackWalker.getCallerClass();
-		final ServiceCaller<ILog> log = new ServiceCaller(pluginName, ILog.class);
-		IStatus status = new Status(IStatus.WARNING, message, null);
+		final ServiceCaller<ILog> log = new ServiceCaller<>(pluginName, ILog.class);
+		IStatus status = new Status(IStatus.WARNING, pluginName.getPackageName(), message);
 		log.call(logger -> logger.log(status));
 	}
 	public static void logDebug(String message) {
 		StackWalker stackWalker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 		final var pluginName = stackWalker.getCallerClass();
-		final ServiceCaller<DebugPlugin> log = new ServiceCaller(pluginName, DebugPlugin.class);
-		IStatus status = new Status(IStatus.WARNING, message, null);
+		final ServiceCaller<DebugPlugin> log = new ServiceCaller<>(pluginName, DebugPlugin.class);
+		IStatus status = new Status(IStatus.WARNING, pluginName.getPackageName(), message);
 
 		log.call(logger -> DebugPlugin.log(status));
 	}
