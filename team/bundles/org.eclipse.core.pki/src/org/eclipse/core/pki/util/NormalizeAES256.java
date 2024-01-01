d144ce78bfa7d4c8ca6ca4c4480e208258d8e764
joe <joe@schiavone.org>
2024-01-01 09:52:59 -0500

    Add encryption

---
+++ team/bundles/org.eclipse.core.pki/src/org/eclipse/core/pki/util/NormalizeAES256.java
@@ -0,0 +1,45 @@
+package com.daggerpoint.uievent;
+
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.spec.KeySpec;
+
+public enum NormalizeAES256 {
+	DECRYPT;
+	private static final int KEY_LENGTH = 256;
+	private static final int ITERATION_COUNT = 65536;
+	public String decrypt(String strToDecrypt, String secretKey, String salt) {
+
+		try {
+
+			byte[] encryptedData = Base64.getDecoder().decode(strToDecrypt);
+			byte[] iv = new byte[16];
+			System.arraycopy(encryptedData, 0, iv, 0, iv.length);
+			IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+			SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+			KeySpec spec = new PBEKeySpec(secretKey.toCharArray(), salt.getBytes(), ITERATION_COUNT, KEY_LENGTH);
+			SecretKey tmp = factory.generateSecret(spec);
+			SecretKeySpec secretKeySpec = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivspec);
+
+			byte[] cipherText = new byte[encryptedData.length - 16];
+			System.arraycopy(encryptedData, 16, cipherText, 0, cipherText.length);
+
+			byte[] decryptedText = cipher.doFinal(cipherText);
+			return new String(decryptedText, "UTF-8");
+		} catch (Exception e) {
+			// Handle the exception properly
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
