aae77240b50bd7888d9388f400e34aa84a6deef0
Michael Bangas <michael.bangas@vector.com>
2024-06-24 09:37:25 +0200

    temporary workaround for gender issues
    temporary workaround for CloseTest#testCloseZipFileWithZipFileUnderneath

--- resources/bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/localstore/RefreshLocalVisitor.java
+++ resources/bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/localstore/RefreshLocalVisitor.java
@@ -14,6 +14,7 @@
  *******************************************************************************/
 package org.eclipse.core.internal.localstore;
 
+import org.eclipse.core.filesystem.ZipFileUtil;
 import org.eclipse.core.internal.resources.Container;
 import org.eclipse.core.internal.resources.File;
 import org.eclipse.core.internal.resources.Folder;
@@ -247,7 +248,7 @@ public class RefreshLocalVisitor implements IUnifiedTreeVisitor, ILocalStoreCons
 				return false;
 			}
 		} else {
-			if (!node.isFolder()) {
+			if (!node.isFolder() && !ZipFileUtil.isOpenZipFile(target.getStore())) {
 				folderToFile(node, target);
 				resourceChanged = true;
 				return false;
