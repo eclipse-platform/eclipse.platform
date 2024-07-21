c74f616892b95012a8f62a2e052e870acee2b072
Hannes Wellmann <wellmann.hannes1@gmx.net>
2024-07-21 12:06:58 +0200

    Use try-with-resources in filesystem FileStore

--- resources/bundles/org.eclipse.core.filesystem/src/org/eclipse/core/internal/filesystem/Policy.java
+++ resources/bundles/org.eclipse.core.filesystem/src/org/eclipse/core/internal/filesystem/Policy.java
@@ -13,8 +13,6 @@
  *******************************************************************************/
 package org.eclipse.core.internal.filesystem;
 
-import java.io.*;
-import java.util.Date;
 import org.eclipse.core.internal.runtime.RuntimeLog;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Status;
@@ -24,20 +22,6 @@ import org.eclipse.core.runtime.Status;
  */
 public class Policy {
 	public static final String PI_FILE_SYSTEM = "org.eclipse.core.filesystem"; //$NON-NLS-1$
-
-	/**
-	 * Print a debug message to the console.
-	 * Pre-pend the message with the current date and the name of the current thread.
-	 */
-	public static void debug(String message) {
-		StringBuilder buffer = new StringBuilder();
-		buffer.append(new Date(System.currentTimeMillis()));
-		buffer.append(" - ["); //$NON-NLS-1$
-		buffer.append(Thread.currentThread().getName());
-		buffer.append("] "); //$NON-NLS-1$
-		buffer.append(message);
-		System.out.println(buffer.toString());
-	}
 
 	public static void error(int code, String message) throws CoreException {
 		error(code, message, null);
@@ -54,27 +38,4 @@ public class Policy {
 		RuntimeLog.log(new Status(severity, PI_FILE_SYSTEM, 1, message, t));
 	}
 
-	/**
-	 * Closes a stream and ignores any resulting exception.
-	 */
-	public static void safeClose(InputStream in) {
-		try {
-			if (in != null)
-				in.close();
-		} catch (IOException e) {
-			//ignore
-		}
-	}
-
-	/**
-	 * Closes a stream and ignores any resulting exception.
-	 */
-	public static void safeClose(OutputStream out) {
-		try {
-			if (out != null)
-				out.close();
-		} catch (IOException e) {
-			//ignore
-		}
-	}
 }
