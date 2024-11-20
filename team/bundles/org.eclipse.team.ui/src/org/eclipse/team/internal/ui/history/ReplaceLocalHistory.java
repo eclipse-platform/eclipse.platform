15e1afbdd55ded791a36969c72cd289ac1664314
Tobias Melcher <tobias.melcher@sap.com>
2024-11-20 12:57:25 +0100

    Show Diff in "replace with local history" Dialog On Selection Change
    - disallow multi selection in "replace with local history" dialog
    - fix layouting issue on MacOS when loading the compare viewers on
    selection change

--- team/bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/history/ReplaceLocalHistory.java
+++ team/bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/history/ReplaceLocalHistory.java
@@ -15,17 +15,29 @@ package org.eclipse.team.internal.ui.history;
 
 import org.eclipse.compare.CompareConfiguration;
 import org.eclipse.compare.CompareUI;
+import org.eclipse.compare.CompareViewerPane;
 import org.eclipse.compare.structuremergeviewer.ICompareInput;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFileState;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Tree;
 import org.eclipse.team.internal.ui.TeamUIMessages;
 import org.eclipse.team.internal.ui.TeamUIPlugin;
 import org.eclipse.team.internal.ui.Utils;
 import org.eclipse.team.ui.history.HistoryPageCompareEditorInput;
 import org.eclipse.team.ui.history.IHistoryPageSource;
+import org.eclipse.ui.part.IPage;
 
 public class ReplaceLocalHistory extends ShowLocalHistory {
 
@@ -54,6 +66,40 @@ public class ReplaceLocalHistory extends ShowLocalHistory {
 				public String getOKButtonLabel() {
 					return TeamUIMessages.ReplaceLocalHistory_0;
 				}
+
+				@Override
+				protected IPage createPage(CompareViewerPane parent, IToolBarManager toolBarManager) {
+					var page = super.createPage(parent, toolBarManager, false);
+					Tree tree = getTree(page);
+					runDefaultSelectionEventOnSelectionChange(tree);
+					return page;
+				}
+
+				private void runDefaultSelectionEventOnSelectionChange(Tree tree) {
+					if (tree == null) {
+						return;
+					}
+					var handler = new NotifyDefaultSelectionOnWidgetSelectedHandler(tree);
+					tree.addSelectionListener(handler);
+					tree.addMouseListener(handler);
+				}
+
+				private Tree getTree(IPage page) {
+					Control control = page.getControl();
+					if (!(control instanceof Composite composite)) {
+						return null;
+					}
+					Control[] children = composite.getChildren();
+					if (children == null) {
+						return null;
+					}
+					for (Control child : children) {
+						if (child instanceof Tree t) {
+							return t;
+						}
+					}
+					return null;
+				}
 				@Override
 				public boolean okPressed() {
 					try {
@@ -75,5 +121,51 @@ public class ReplaceLocalHistory extends ShowLocalHistory {
 	@Override
 	protected String getPromptTitle() {
 		return TeamUIMessages.ReplaceLocalHistory_1;
+	}
+
+	private static final class NotifyDefaultSelectionOnWidgetSelectedHandler extends SelectionAdapter
+			implements MouseListener {
+		private boolean mouseDownPressed = false;
+		private Runnable runOnMouseUp;
+		private final Tree tree;
+
+		public NotifyDefaultSelectionOnWidgetSelectedHandler(Tree tree) {
+			this.tree = tree;
+		}
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			if (tree.getSelectionCount() != 1) {
+				return;
+			}
+			Runnable r = () -> {
+				Event event = new Event();
+				event.item = e.item;
+				tree.notifyListeners(SWT.DefaultSelection, event);
+			};
+			if (mouseDownPressed) {
+				// run DefaultSelection event after mouseUp
+				runOnMouseUp = r;
+				return;
+			}
+			r.run();
+		}
+
+		@Override
+		public void mouseDoubleClick(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseDown(MouseEvent e) {
+			mouseDownPressed = true;
+		}
+
+		@Override
+		public void mouseUp(MouseEvent e) {
+			mouseDownPressed = false;
+			if (runOnMouseUp != null) {
+				runOnMouseUp.run();
+				runOnMouseUp = null;
+			}
+		}
 	}
 }
