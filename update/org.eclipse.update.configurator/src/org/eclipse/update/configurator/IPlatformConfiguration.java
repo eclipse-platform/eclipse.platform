34516abf9f534a38759b8a33ab7e2e30e1846248
Christoph Läubrich <laeubi@laeubi-soft.de>
2024-12-15 07:03:12 +0100

    Mark IPlatformConfiguration/Factory for removal
    These interfaces are currently only used by ConfiguratorUtils that is
    already marked for removal on 2024-03

--- update/org.eclipse.update.configurator/src/org/eclipse/update/configurator/IPlatformConfiguration.java
+++ update/org.eclipse.update.configurator/src/org/eclipse/update/configurator/IPlatformConfiguration.java
@@ -36,7 +36,7 @@ import java.net.URL;
  * @deprecated The org.eclipse.update component has been replaced by Equinox p2.
  * This API will be deleted in a future release. See bug 311590 for details.
  */
-@Deprecated
+@Deprecated(forRemoval = true, since = "2025-03")
 public interface IPlatformConfiguration {
 
 	/**
--- update/org.eclipse.update.configurator/src/org/eclipse/update/configurator/IPlatformConfigurationFactory.java
+++ update/org.eclipse.update.configurator/src/org/eclipse/update/configurator/IPlatformConfigurationFactory.java
@@ -27,7 +27,7 @@ import java.net.URL;
  * @deprecated The org.eclipse.update component has been replaced by Equinox p2.
  * This API will be deleted in a future release. See bug 311590 for details.
  */
-@Deprecated
+@Deprecated(forRemoval = true, since = "2025-03")
 public interface IPlatformConfigurationFactory {
 	/**
 	 * Returns the current platform configuration.
