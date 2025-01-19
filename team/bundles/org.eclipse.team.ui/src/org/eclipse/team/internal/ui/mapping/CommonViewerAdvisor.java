2177bdfd0fc2d86885cb5dcaa8a7a0e11b02db28
Александър Куртаков <akurtakov@gmail.com>
2025-01-19 17:38:26 +0200

    Move away of deprecated ViewerSorter
    Use ViewerComparator methods.

--- team/bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/mapping/CommonViewerAdvisor.java
+++ team/bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/mapping/CommonViewerAdvisor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2025 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -268,8 +268,8 @@ public class CommonViewerAdvisor extends AbstractTreeViewerAdvisor implements IN
 	 */
 	private static CommonViewer createViewer(Composite parent, final ISynchronizePageConfiguration configuration, IEmptyTreeListener listener) {
 		final CommonViewer v = new NavigableCommonViewer(configuration.getViewerId(), parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL, listener);
-		v.setSorter(new CommonViewerSorter());
-		v.setSorter(new TeamViewerSorter((CommonViewerSorter)v.getSorter()));
+		v.setComparator(new CommonViewerSorter());
+		v.setComparator(new TeamViewerSorter((CommonViewerSorter) v.getSorter()));
 		ISynchronizationScope scope = getScope(configuration);
 		bindTeamContentProviders(v);
 		scope.addScopeChangeListener((scope1, newMappings, newTraversals) -> {
