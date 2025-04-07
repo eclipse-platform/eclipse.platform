97b8139bc9c39ae50471e484a09f71a1c647e143
Michael Bangas <michael.bangas@vector.com>
2025-04-07 14:16:08 +0200

    add all svgs to org.eclipse.team.ui
    This commit adds SVGs for all icons in the bundle `org.eclipse.team.ui`.

--- team/bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/ITeamUIImages.java
+++ team/bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/ITeamUIImages.java
@@ -24,47 +24,47 @@ public interface ITeamUIImages {
 	/**
 	 * Identifies an incoming change.
 	 */
-	public final String IMG_DLG_SYNC_INCOMING = "elcl16/incom_synch.png"; //$NON-NLS-1$
+	public final String IMG_DLG_SYNC_INCOMING = "elcl16/incom_synch.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies an outgoing change.
 	 */
-	public final String IMG_DLG_SYNC_OUTGOING = "elcl16/outgo_synch.png"; //$NON-NLS-1$
+	public final String IMG_DLG_SYNC_OUTGOING = "elcl16/outgo_synch.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies a conflicting change.
 	 */
-	public final String IMG_DLG_SYNC_CONFLICTING = "elcl16/conflict_synch.png"; //$NON-NLS-1$
+	public final String IMG_DLG_SYNC_CONFLICTING = "elcl16/conflict_synch.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies a refresh action.
 	 */
-	public final String IMG_REFRESH = "elcl16/refresh.png"; //$NON-NLS-1$
+	public final String IMG_REFRESH = "elcl16/refresh.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies a refresh with repository action.
 	 */
-	public final String IMG_REFRESH_REMOTE = "elcl16/refresh_remote.png"; //$NON-NLS-1$
+	public final String IMG_REFRESH_REMOTE = "elcl16/refresh_remote.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies a link with action.
 	 */
-	public final String IMG_LINK_WITH = "elcl16/synced.png"; //$NON-NLS-1$
+	public final String IMG_LINK_WITH = "elcl16/synced.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies an ignore white space action.
 	 */
-	public final String IMG_IGNORE_WHITESPACE = "elcl16/ignorews_edit.png"; //$NON-NLS-1$
+	public final String IMG_IGNORE_WHITESPACE = "elcl16/ignorews_edit.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies a collapse all action.
 	 */
-	public final String IMG_COLLAPSE_ALL = "elcl16/collapseall.png"; //$NON-NLS-1$
+	public final String IMG_COLLAPSE_ALL = "elcl16/collapseall.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies the incoming mode for synchronizing.
 	 */
-	public final String IMG_SYNC_MODE_CATCHUP = "elcl16/catchup_rls.png"; //$NON-NLS-1$
+	public final String IMG_SYNC_MODE_CATCHUP = "elcl16/catchup_rls.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies the outgoing mode for synchronizing.
 	 */
-	public final String IMG_SYNC_MODE_RELEASE = "elcl16/release_rls.png"; //$NON-NLS-1$
+	public final String IMG_SYNC_MODE_RELEASE = "elcl16/release_rls.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies the both mode for synchronizing.
 	 */
-	public final String IMG_SYNC_MODE_FREE = "elcl16/catchuprelease_rls.png"; //$NON-NLS-1$
+	public final String IMG_SYNC_MODE_FREE = "elcl16/catchuprelease_rls.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies an incoming change.
 	 */
@@ -112,87 +112,87 @@ public interface ITeamUIImages {
 	/**
 	 * Identifies an incoming change.
 	 */
-	public final String IMG_DLG_SYNC_INCOMING_ENABLED = "elcl16/incom_synch.png"; //$NON-NLS-1$
+	public final String IMG_DLG_SYNC_INCOMING_ENABLED = "elcl16/incom_synch.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies an outgoing change.
 	 */
-	public final String IMG_DLG_SYNC_OUTGOING_ENABLED = "elcl16/outgo_synch.png"; //$NON-NLS-1$
+	public final String IMG_DLG_SYNC_OUTGOING_ENABLED = "elcl16/outgo_synch.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies a conflicting change.
 	 */
-	public final String IMG_DLG_SYNC_CONFLICTING_ENABLED = "elcl16/conflict_synch.png"; //$NON-NLS-1$
+	public final String IMG_DLG_SYNC_CONFLICTING_ENABLED = "elcl16/conflict_synch.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies a refresh action.
 	 */
-	public final String IMG_REFRESH_ENABLED = "elcl16/refresh.png"; //$NON-NLS-1$
+	public final String IMG_REFRESH_ENABLED = "elcl16/refresh.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies a refresh with repository action.
 	 */
-	public final String IMG_REFRESH_REMOTE_ENABLED = "elcl16/refresh_remote.png"; //$NON-NLS-1$
+	public final String IMG_REFRESH_REMOTE_ENABLED = "elcl16/refresh_remote.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies a link with action.
 	 */
-	public final String IMG_LINK_WITH_ENABLED = "elcl16/synced.png"; //$NON-NLS-1$
+	public final String IMG_LINK_WITH_ENABLED = "elcl16/synced.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies an ignore white space action.
 	 */
-	public final String IMG_IGNORE_WHITESPACE_ENABLED = "elcl16/ignorews_edit.png"; //$NON-NLS-1$
+	public final String IMG_IGNORE_WHITESPACE_ENABLED = "elcl16/ignorews_edit.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies a collapse all action.
 	 */
-	public final String IMG_COLLAPSE_ALL_ENABLED = "elcl16/collapseall.png"; //$NON-NLS-1$
+	public final String IMG_COLLAPSE_ALL_ENABLED = "elcl16/collapseall.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies the incoming mode for synchronizing.
 	 */
-	public final String IMG_SYNC_MODE_CATCHUP_ENABLED = "elcl16/catchup_rls.png"; //$NON-NLS-1$
+	public final String IMG_SYNC_MODE_CATCHUP_ENABLED = "elcl16/catchup_rls.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies the outgoing mode for synchronizing.
 	 */
-	public final String IMG_SYNC_MODE_RELEASE_ENABLED = "elcl16/release_rls.png"; //$NON-NLS-1$
+	public final String IMG_SYNC_MODE_RELEASE_ENABLED = "elcl16/release_rls.svg"; //$NON-NLS-1$
 	/**
 	 * Identifies the both mode for synchronizing.
 	 */
-	public final String IMG_SYNC_MODE_FREE_ENABLED = "elcl16/catchuprelease_rls.png"; //$NON-NLS-1$
+	public final String IMG_SYNC_MODE_FREE_ENABLED = "elcl16/catchuprelease_rls.svg"; //$NON-NLS-1$
 	/**
 	 * Share projects banner.
 	 */
-	public final String IMG_WIZBAN_SHARE = "wizban/share_wizban.png"; //$NON-NLS-1$
+	public final String IMG_WIZBAN_SHARE = "wizban/share_wizban.svg"; //$NON-NLS-1$
 	/**
 	 * Import project set banner.
 	 */
-	public final String IMG_PROJECTSET_IMPORT_BANNER = "wizban/import_projectset_wizban.png"; //$NON-NLS-1$
+	public final String IMG_PROJECTSET_IMPORT_BANNER = "wizban/import_projectset_wizban.svg"; //$NON-NLS-1$
 	/**
 	 * Export project set banner.
 	 */
-	public final String IMG_PROJECTSET_EXPORT_BANNER = "wizban/export_projectset_wizban.png"; //$NON-NLS-1$
+	public final String IMG_PROJECTSET_EXPORT_BANNER = "wizban/export_projectset_wizban.svg"; //$NON-NLS-1$
 	/**
 	 * Icon for authentication dialogs.
 	 */
-	public final String IMG_KEY_LOCK = "wizban/keylock.png"; //$NON-NLS-1$
+	public final String IMG_KEY_LOCK = "wizban/keylock.svg"; //$NON-NLS-1$
 	/**
 	 * Pinned icon
 	 */
-	public final String IMG_PINNED = "elcl16/pin.png";  //$NON-NLS-1$
+	public final String IMG_PINNED = "elcl16/pin.svg"; //$NON-NLS-1$
 	/**
 	 * Previous history
 	 */
-	public final String IMG_PREVIOUS = "elcl16/prev_nav.png"; //$NON-NLS-1$
+	public final String IMG_PREVIOUS = "elcl16/prev_nav.svg"; //$NON-NLS-1$
 
 	//objects
-	public final String IMG_SITE_ELEMENT = "elcl16/site_element.png"; //$NON-NLS-1$
-	public final String IMG_CHANGE_FILTER = "elcl16/change_filter.png"; //$NON-NLS-1$
-	public final String IMG_COMPRESSED_FOLDER = "obj/compressed_folder_obj.png"; //$NON-NLS-1$
-	public final String IMG_HIERARCHICAL = "elcl16/hierarchicalLayout.png"; //$NON-NLS-1$
-	public final String IMG_FLAT = "elcl16/flatLayout.png"; //$NON-NLS-1$
-	public final String IMG_SYNC_VIEW = "eview16/synch_synch.png"; //$NON-NLS-1$
-	public final String IMG_CHANGE_SET = "obj/changeset_obj.png"; //$NON-NLS-1$
+	public final String IMG_SITE_ELEMENT = "elcl16/site_element.svg"; //$NON-NLS-1$
+	public final String IMG_CHANGE_FILTER = "elcl16/change_filter.svg"; //$NON-NLS-1$
+	public final String IMG_COMPRESSED_FOLDER = "obj/compressed_folder_obj.svg"; //$NON-NLS-1$
+	public final String IMG_HIERARCHICAL = "elcl16/hierarchicalLayout.svg"; //$NON-NLS-1$
+	public final String IMG_FLAT = "elcl16/flatLayout.svg"; //$NON-NLS-1$
+	public final String IMG_SYNC_VIEW = "eview16/synch_synch.svg"; //$NON-NLS-1$
+	public final String IMG_CHANGE_SET = "obj/changeset_obj.svg"; //$NON-NLS-1$
 
 	/**
 	 * Images used by the Local History Page
 	 */
-	public final String IMG_COMPARE_VIEW = "eview16/compare_view.png"; //$NON-NLS-1$
-	public final String IMG_DATES_CATEGORY = "obj/dates.png"; //$NON-NLS-1$
-	public final String IMG_LOCALREVISION_TABLE = "obj/local_entry_tbl.png"; //$NON-NLS-1$
+	public final String IMG_COMPARE_VIEW = "eview16/compare_view.svg"; //$NON-NLS-1$
+	public final String IMG_DATES_CATEGORY = "obj/dates.svg"; //$NON-NLS-1$
+	public final String IMG_LOCALREVISION_TABLE = "obj/local_entry_tbl.svg"; //$NON-NLS-1$
 
 
 }
