ab649b67e7bfdfbd8ca45a72235ac1710d41a9d5
Aleksandar Kurtakov <akurtakov@gmail.com>
2025-04-16 10:19:27 +0300

    Remove unused browser helper code
    Nowhere in the code checks for Konqueror or Gecko is done so we would
    better reduce the luggage.

--- ua/org.eclipse.help.webapp/src/org/eclipse/help/internal/webapp/data/UrlUtil.java
+++ ua/org.eclipse.help.webapp/src/org/eclipse/help/internal/webapp/data/UrlUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corporation and others.
+ * Copyright (c) 2000, 2025 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -268,22 +268,6 @@ public class UrlUtil {
 				|| request.getParameter("bot") != null;//$NON-NLS-1$
 	}
 
-	public static boolean isGecko(HttpServletRequest request) {
-		String agent = request.getHeader("User-Agent"); //$NON-NLS-1$
-		return isGecko(agent);
-	}
-
-	public static boolean isGecko(String agent) {
-		if (agent==null)
-			return false;
-		agent=agent.toLowerCase(Locale.ENGLISH);
-		// sample substring Gecko/20020508
-		if (agent.contains("like gecko")) { //$NON-NLS-1$
-			return false;
-		}
-		return agent.contains("gecko"); //$NON-NLS-1$
-	}
-
 	public static boolean isIE(HttpServletRequest request) {
 		String agent = request.getHeader("User-Agent"); //$NON-NLS-1$
 		return isIE(agent);
@@ -328,18 +312,6 @@ public class UrlUtil {
 		if (end <= start)
 			return "0"; //$NON-NLS-1$
 		return agent.substring(start, end);
-	}
-
-	public static boolean isKonqueror(HttpServletRequest request) {
-		String agent = request.getHeader("User-Agent"); //$NON-NLS-1$
-		return isKonqueror(agent);
-	}
-
-	public static boolean isKonqueror(String agent) {
-		if (agent==null)
-			return false;
-		agent=agent.toLowerCase(Locale.ENGLISH);
-		return agent.contains("konqueror"); //$NON-NLS-1$
 	}
 
 	/**
