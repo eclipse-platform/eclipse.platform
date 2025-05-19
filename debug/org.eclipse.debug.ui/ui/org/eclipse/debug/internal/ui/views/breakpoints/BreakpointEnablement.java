fe670a93ddc8ff22f980ec756527b024cac46703
sougandhs <sougandh.s@ibm.com>
2025-05-19 10:52:52 +0530

    Add grouping for breakpoints by Enablement
    This commit adds a new grouping option in the Debug view to organize
    breakpoints based on whether they are enabled or disabled. Enabled
    breakpoints are shown at the top, followed by disabled ones. This helps
    users quickly find and manage breakpoints without excessive scrolling.

---
+++ debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/breakpoints/BreakpointEnablement.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.debug.internal.ui.views.breakpoints;
+
+import org.eclipse.core.resources.IMarkerDelta;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.IBreakpointListener;
+import org.eclipse.debug.core.model.IBreakpoint;
+import org.eclipse.debug.ui.AbstractBreakpointOrganizerDelegate;
+import org.eclipse.debug.ui.BreakpointTypeCategory;
+
+/**
+ * Breakpoint organizers for breakpoint types.
+ *
+ * @since 3.1
+ */
+public class BreakpointEnablement extends AbstractBreakpointOrganizerDelegate implements IBreakpointListener {
+	private final BreakpointTypeCategory ENABLED = new BreakpointTypeCategory("Enabled", true, 0); //$NON-NLS-1$
+	private final BreakpointTypeCategory DISABLED = new BreakpointTypeCategory("Disabled", true, 1); //$NON-NLS-1$
+
+	public BreakpointEnablement() {
+		DebugPlugin.getDefault().getBreakpointManager().addBreakpointListener(this);
+	}
+
+	@Override
+	public IAdaptable[] getCategories(IBreakpoint breakpoint) {
+		try {
+			IAdaptable[] categories;
+			if (breakpoint.isEnabled()) {
+				categories = new IAdaptable[] { ENABLED };
+			} else {
+				categories = new IAdaptable[] { DISABLED };
+			}
+			return categories;
+		} catch (CoreException e) {
+			DebugPlugin.log(e);
+		}
+		return null;
+	}
+
+	@Override
+	public void dispose() {
+		DebugPlugin.getDefault().getBreakpointManager().removeBreakpointListener(this);
+	}
+
+
+	@Override
+	public void breakpointAdded(IBreakpoint breakpoint) {
+	}
+
+	@Override
+	public void breakpointRemoved(IBreakpoint breakpoint, IMarkerDelta delta) {
+	}
+
+	@Override
+	public void breakpointChanged(IBreakpoint breakpoint, IMarkerDelta delta) {
+		fireCategoryChanged(ENABLED);
+	}
+}
