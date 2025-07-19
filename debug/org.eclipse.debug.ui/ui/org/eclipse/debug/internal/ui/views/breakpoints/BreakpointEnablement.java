253db6ff1b8d46c3b4504760a1dd36af1c5bdd69
sougandhs <sougandh.s@ibm.com>
2025-07-19 06:36:00 +0530

    Optimize BreakpointEnablement
    - Implement IBreakpointsListener instead of IBreakpointListener to
    enable batch processing of breakpoint changes.
    - Avoid unnecessary category change events for breakpoint
    enablement.
    
    Fixes : https://github.com/eclipse-platform/eclipse.platform/issues/2062

--- debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/breakpoints/BreakpointEnablement.java
+++ debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/breakpoints/BreakpointEnablement.java
@@ -13,11 +13,15 @@
  *******************************************************************************/
 package org.eclipse.debug.internal.ui.views.breakpoints;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 import org.eclipse.core.resources.IMarkerDelta;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.debug.core.DebugPlugin;
-import org.eclipse.debug.core.IBreakpointListener;
+import org.eclipse.debug.core.IBreakpointsListener;
 import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.debug.ui.AbstractBreakpointOrganizerDelegate;
 import org.eclipse.debug.ui.BreakpointTypeCategory;
@@ -26,18 +30,22 @@ import org.eclipse.debug.ui.BreakpointTypeCategory;
  * Breakpoint organizers for breakpoint types based on breakpoint enablement
  * state.
  */
-public class BreakpointEnablement extends AbstractBreakpointOrganizerDelegate implements IBreakpointListener {
+public class BreakpointEnablement extends AbstractBreakpointOrganizerDelegate implements IBreakpointsListener {
 
 	private final BreakpointTypeCategory ENABLED = new BreakpointTypeCategory("Enabled", 0); //$NON-NLS-1$
 	private final BreakpointTypeCategory DISABLED = new BreakpointTypeCategory("Disabled", 1); //$NON-NLS-1$
 
+	private final Map<IBreakpoint, Boolean> breakpointCache;
+
 	public BreakpointEnablement() {
 		DebugPlugin.getDefault().getBreakpointManager().addBreakpointListener(this);
+		breakpointCache = Collections.synchronizedMap(new WeakHashMap<>());
 	}
 
 	@Override
 	public void dispose() {
 		DebugPlugin.getDefault().getBreakpointManager().removeBreakpointListener(this);
+		breakpointCache.clear();
 	}
 
 	@Override
@@ -49,6 +57,7 @@ public class BreakpointEnablement extends AbstractBreakpointOrganizerDelegate im
 			} else {
 				categories = new IAdaptable[] { DISABLED };
 			}
+			breakpointCache.put(breakpoint, breakpoint.isEnabled());
 			return categories;
 		} catch (CoreException e) {
 			DebugPlugin.log(e);
@@ -57,15 +66,40 @@ public class BreakpointEnablement extends AbstractBreakpointOrganizerDelegate im
 	}
 
 	@Override
-	public void breakpointAdded(IBreakpoint breakpoint) {
+	public void breakpointsAdded(IBreakpoint[] breakpoints) {
+		for (IBreakpoint breakpoint : breakpoints) {
+			try {
+				breakpointCache.put(breakpoint, breakpoint.isEnabled());
+			} catch (CoreException e) {
+				DebugPlugin.log(e);
+			}
+		}
 	}
 
 	@Override
-	public void breakpointRemoved(IBreakpoint breakpoint, IMarkerDelta delta) {
+	public void breakpointsRemoved(IBreakpoint[] breakpoints, IMarkerDelta[] deltas) {
+		for (IBreakpoint breakpoint : breakpoints) {
+			breakpointCache.remove(breakpoint);
+		}
 	}
 
 	@Override
-	public void breakpointChanged(IBreakpoint breakpoint, IMarkerDelta delta) {
-		fireCategoryChanged(ENABLED);
+	public void breakpointsChanged(IBreakpoint[] breakpoints, IMarkerDelta[] deltas) {
+		boolean updateCategories = false;
+		for (IBreakpoint breakpoint : breakpoints) {
+			try {
+				boolean isEnabled = breakpoint.isEnabled();
+				Boolean wasEnabled = breakpointCache.get(breakpoint);
+				if (wasEnabled == null || wasEnabled != isEnabled) {
+					breakpointCache.put(breakpoint, isEnabled);
+					updateCategories = true;
+				}
+			} catch (CoreException e) {
+				DebugPlugin.log(e);
+			}
+		}
+		if (updateCategories) {
+			fireCategoryChanged(ENABLED);
+		}
 	}
 }
