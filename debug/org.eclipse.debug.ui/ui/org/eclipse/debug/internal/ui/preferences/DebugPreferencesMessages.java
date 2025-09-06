8380447e0b134a962a297d2fff42b8f5791344c3
sougandhs <sougandh.s@ibm.com>
2025-09-06 09:35:05 +0530

    Add configurable elapsed time display with format customization
    This commit introduces a Combo selection in Console settings to choose
    different formatting options for elapsed time shown on the console
    output or disable it entirely
    
    fixes : https://github.com/eclipse-platform/eclipse.platform/issues/2112

--- debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/preferences/DebugPreferencesMessages.java
+++ debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/preferences/DebugPreferencesMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2020 IBM Corporation and others.
+ *  Copyright (c) 2000, 2025 IBM Corporation and others.
  *
  *  This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License 2.0
@@ -218,5 +218,13 @@ public class DebugPreferencesMessages extends NLS {
 
 	public static String RunDebugPropertiesPage_0;
 
+
+	public static String ConsoleDefaultElapsedTimeFormat;
+
+	public static String ConsoleElapsedTimeLabel;
+
+	public static String ConsoleElapsedTimeToolTip;
+
+	public static Object ConsoleDisableElapsedTime;
 
 }
