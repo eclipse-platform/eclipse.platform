22ad8626a21c9d3fc751bdd6d83d69c54fa8521d
sougandhs <sougandh.s@ibm.com>
2025-09-06 09:35:05 +0530

    Add configurable elapsed time display with format customization
    This commit introduces a Combo selection in Console settings to choose
    different formatting options for elapsed time shown on the console
    output or disable it entirely
    
    fixes : https://github.com/eclipse-platform/eclipse.platform/issues/2112

--- debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/DebugUIPreferenceInitializer.java
+++ debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/DebugUIPreferenceInitializer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2004, 2020 IBM Corporation and others.
+ *  Copyright (c) 2004, 2025 IBM Corporation and others.
  *
  *  This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License 2.0
@@ -15,6 +15,7 @@
 package org.eclipse.debug.internal.ui;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.debug.internal.ui.preferences.DebugPreferencesMessages;
 import org.eclipse.debug.internal.ui.preferences.IDebugPreferenceConstants;
 import org.eclipse.debug.internal.ui.views.memory.MemoryViewUtil;
 import org.eclipse.debug.ui.IDebugUIConstants;
@@ -84,6 +85,8 @@ public class DebugUIPreferenceInitializer extends AbstractPreferenceInitializer 
 		prefs.setDefault(IDebugPreferenceConstants.CONSOLE_TAB_WIDTH, 8);
 		prefs.setDefault(IDebugPreferenceConstants.CONSOLE_INTERPRET_CONTROL_CHARACTERS, false);
 		prefs.setDefault(IDebugPreferenceConstants.CONSOLE_INTERPRET_CR_AS_CONTROL_CHARACTER, true);
+		prefs.setDefault(IDebugPreferenceConstants.CONSOLE_ELAPSED_FORMAT,
+				DebugPreferencesMessages.ConsoleDefaultElapsedTimeFormat);
 
 		// console colors
 		setThemeBasedPreferences(prefs, false);
