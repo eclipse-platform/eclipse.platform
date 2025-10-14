48d731412a67f4e064fcb854b796c51e6e988669
sougandhs <sougandh.s@ibm.com>
2025-10-14 17:23:42 +0530

    Improved Expression Copy
    This commit improves Copy Expression by excluding eclipse ui values such
    as pending, error and disabled

--- debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/expressions/CopyExpressionsToClipboardActionDelegate.java
+++ debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/expressions/CopyExpressionsToClipboardActionDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2017 Bachmann electronic GmbH and others.
+ *  Copyright (c) 2017, 2025 Bachmann electronic GmbH and others.
  *
  *  This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License 2.0
@@ -10,15 +10,17 @@
  *
  *  Contributors:
  *     Bachmann electronic GmbH - initial API and implementation
+ *     IBM Corporation - Exclude Expression UI values while copying
  *******************************************************************************/
 package org.eclipse.debug.internal.ui.actions.expressions;
 
+import org.eclipse.debug.internal.core.WatchExpression;
+import org.eclipse.debug.internal.ui.DebugUIMessages;
 import org.eclipse.debug.internal.ui.viewers.model.VirtualCopyToClipboardActionDelegate;
 
 public class CopyExpressionsToClipboardActionDelegate extends VirtualCopyToClipboardActionDelegate {
 
 	private static final String QUOTE = "\""; //$NON-NLS-1$
-
 	@Override
 	protected String trimLabel(String rawLabel) {
 		String label = super.trimLabel(rawLabel);
@@ -30,6 +32,32 @@ public class CopyExpressionsToClipboardActionDelegate extends VirtualCopyToClipb
 		}
 		if (label.endsWith(QUOTE)) {
 			label = label.substring(0, label.length() - 1);
+		}
+		return label;
+	}
+
+	@Override
+	protected String exludeLabels(Object itemData, String label) {
+		if (itemData instanceof WatchExpression watchExp) {
+			if (watchExp.isPending() || watchExp.hasErrors()) {
+				if (label.equals(DebugUIMessages.DefaultLabelProvider_12)
+						|| label.contains(DebugUIMessages.DefaultLabelProvider_13)) {
+					return null;
+				}
+			}
+			if (!watchExp.isEnabled()) {
+				if (label.equals(DebugUIMessages.DefaultLabelProvider_15)) {
+					return null;
+				}
+				if (label.contains(DebugUIMessages.DefaultLabelProvider_15)) {
+					int index = label.lastIndexOf(DebugUIMessages.DefaultLabelProvider_15);
+					if (index != -1) {
+						label = label.substring(0, index)
+								+ label.substring(index + DebugUIMessages.DefaultLabelProvider_15.length());
+					}
+				}
+			}
+			return label;
 		}
 		return label;
 	}
