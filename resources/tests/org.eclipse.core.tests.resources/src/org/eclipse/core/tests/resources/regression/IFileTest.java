cd85fea4e4f75bbd58a7a533384aacf01e91cc86
Vasili Gulevich <vasili.gulevich@xored.com>
2025-11-26 20:35:56 +0400

    Do not throw RuntimeException from IFile.getContents(false) #2290

--- resources/tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/regression/IFileTest.java
+++ resources/tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/regression/IFileTest.java
@@ -13,6 +13,7 @@
  *******************************************************************************/
 package org.eclipse.core.tests.resources.regression;
 
+import static java.lang.System.currentTimeMillis;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.eclipse.core.resources.ResourcesPlugin.getWorkspace;
 import static org.eclipse.core.tests.resources.ResourceTestUtil.createInWorkspace;
@@ -24,6 +25,9 @@ import static org.junit.Assume.assumeTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
@@ -31,7 +35,10 @@ import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceStatus;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform.OS;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.tests.resources.util.WorkspaceResetExtension;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -123,6 +130,84 @@ public class IFileTest {
 
 		// try setting the description -- shouldn't fail
 		project.setDescription(desc, createTestMonitor());
+	}
+
+	/**
+	 * Do not throw RuntimeException when accessing a deleted file
+	 */
+	@Test
+	public void testIssue2290() throws CoreException, InterruptedException {
+		IProject project = getWorkspace().getRoot().getProject("MyProject");
+		IFile subject = project.getFile("subject.txt");
+		Job createDelete = Job.create("Create/delete", monitor -> {
+			try {
+				while (!monitor.isCanceled()) {
+					createInWorkspace(subject);
+					while (!monitor.isCanceled()) {
+						try {
+							subject.delete(true, monitor);
+							break;
+						} catch (CoreException e) {
+							// On Windows, files opened for reading can't be deleted, try again
+							if (e.getStatus().getCode() != IResourceStatus.FAILED_DELETE_LOCAL) {
+								throw e;
+							}
+						}
+					}
+				}
+			} catch (CoreException e) {
+				return e.getStatus();
+			}
+			return Status.OK_STATUS;
+		});
+		createDelete.setPriority(Job.INTERACTIVE);
+
+		long stop = currentTimeMillis() + 1000;
+		try {
+			createDelete.schedule();
+			while (currentTimeMillis() < stop) {
+				assertContentAccessibleOrNotFound(subject); // should not throw
+			}
+		} finally {
+			createDelete.cancel();
+			createDelete.join();
+			IStatus result = createDelete.getResult();
+			if (!result.isOK()) {
+				throw new CoreException(result);
+			}
+		}
+	}
+
+	private void assertContentAccessibleOrNotFound(IFile file) {
+		try (InputStream contents = file.getContents(false)) {
+			contents.transferTo(OutputStream.nullOutputStream());
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		} catch (CoreException e) {
+			switch (e.getStatus().getCode()) {
+			case IResourceStatus.RESOURCE_NOT_LOCAL:
+			case IResourceStatus.RESOURCE_NOT_FOUND:
+			case IResourceStatus.FAILED_READ_LOCAL:
+			case IResourceStatus.OUT_OF_SYNC_LOCAL:
+				break;
+			default:
+				throw new AssertionError(e);
+			}
+		}
+		try (InputStream contents = file.getContents(true)) {
+			contents.transferTo(OutputStream.nullOutputStream());
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		} catch (CoreException e) {
+			switch (e.getStatus().getCode()) {
+			case IResourceStatus.RESOURCE_NOT_LOCAL:
+			case IResourceStatus.RESOURCE_NOT_FOUND:
+			case IResourceStatus.FAILED_READ_LOCAL:
+				break;
+			default:
+				throw new AssertionError(e);
+			}
+		}
 	}
 
 }
