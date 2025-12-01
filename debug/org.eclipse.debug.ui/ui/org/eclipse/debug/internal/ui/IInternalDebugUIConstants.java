a329aa5f4a7665eb8f5e28cf9beb08877d1f12cd
sougandhs <sougandh.s@ibm.com>
2025-12-01 14:21:15 +0530

    Improve Paste Expressions context
    Enhance Expressions View paste functionality with context menu entry,
    multi-line paste prompt, and a Run/Debug preference for paste behavior

--- debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/IInternalDebugUIConstants.java
+++ debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/IInternalDebugUIConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2025 IBM Corporation and others.
+ * Copyright (c) 2000, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -464,5 +464,23 @@ public interface IInternalDebugUIConstants {
 	 *
 	 */
 	String PREF_SKIP_ALL_BREAKPOINTS_PROMPT = IDebugUIConstants.PLUGIN_ID + ".DisableSkipAllBreakpointsOnLaunch"; //$NON-NLS-1$
+
+	/**
+	 * String indicating always prompt on pasting multi-line expression
+	 *
+	 */
+	String EXPRESSION_PASTE_PROMPT = "org.eclipse.debug.ui.expression.paste.prompt"; //$NON-NLS-1$
+
+	/**
+	 * String indicating always paste as combined single expression
+	 *
+	 */
+	String EXPRESSION_PASTE_AS_SINGLE = "org.eclipse.debug.ui.expression.paste.single"; //$NON-NLS-1$
+
+	/**
+	 * String indicating always paste as multiple expressions
+	 *
+	 */
+	String EXPRESSION_PASTE_AS_MULTI = "org.eclipse.debug.ui.expression.paste.multi"; //$NON-NLS-1$
 
 }
