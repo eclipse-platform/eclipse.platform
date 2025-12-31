7afd0330498ed37f015ed34bdd1c7ef44f7a6464
Heiko Klare <heiko.klare@vector.com>
2025-12-31 15:42:54 +0100

    Remove empty Ant UI test class and unused utility methods
    The removed methods are unused and the class has always been empty /
    commented out.

--- ant/org.eclipse.ant.tests.ui/Ant Editor Tests/org/eclipse/ant/tests/ui/editor/performance/EditorTestHelper.java
+++ ant/org.eclipse.ant.tests.ui/Ant Editor Tests/org/eclipse/ant/tests/ui/editor/performance/EditorTestHelper.java
@@ -14,12 +14,7 @@
 package org.eclipse.ant.tests.ui.editor.performance;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.jobs.IJobManager;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
@@ -27,8 +22,6 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
-import org.eclipse.ui.texteditor.IDocumentProvider;
-import org.eclipse.ui.texteditor.ITextEditor;
 
 /**
  * @since 3.1
@@ -49,19 +42,6 @@ public class EditorTestHelper {
 			runEventQueue(part);
 		}
 		return part;
-	}
-
-	public static IDocument getDocument(ITextEditor editor) {
-		IDocumentProvider provider = editor.getDocumentProvider();
-		IEditorInput input = editor.getEditorInput();
-		return provider.getDocument(input);
-	}
-
-	public static void revertEditor(ITextEditor editor, boolean runEventQueue) {
-		editor.doRevertToSaved();
-		if (runEventQueue) {
-			runEventQueue(editor);
-		}
 	}
 
 	public static void closeAllEditors() {
@@ -105,28 +85,6 @@ public class EditorTestHelper {
 		return window != null ? window.getActivePage() : null;
 	}
 
-	public static Display getActiveDisplay() {
-		IWorkbenchWindow window = getActiveWorkbenchWindow();
-		return window != null ? window.getShell().getDisplay() : null;
-	}
-
-	public static boolean calmDown(long minTime, long maxTime, long intervalTime) {
-		long startTime = System.currentTimeMillis() + minTime;
-		runEventQueue();
-		while (System.currentTimeMillis() < startTime) {
-			runEventQueue(intervalTime);
-		}
-
-		long endTime = maxTime > 0 ? System.currentTimeMillis() + maxTime : Long.MAX_VALUE;
-		boolean calm = isCalm();
-		while (!calm && System.currentTimeMillis() < endTime) {
-			runEventQueue(intervalTime);
-			calm = isCalm();
-		}
-		// System.out.println("--------------------------------------------------");
-		return calm;
-	}
-
 	public static void sleep(int intervalTime) {
 		try {
 			Thread.sleep(intervalTime);
@@ -136,21 +94,4 @@ public class EditorTestHelper {
 		}
 	}
 
-	public static boolean isCalm() {
-		IJobManager jobManager = Job.getJobManager();
-		for (Job job : jobManager.find(null)) {
-			int state = job.getState();
-			// System.out.println(job.getName() + ": " + getStateName(state));
-			if (state == Job.RUNNING || state == Job.WAITING) {
-				// System.out.println();
-				return false;
-			}
-		}
-		// System.out.println();
-		return true;
-	}
-
-	public static void bringToTop() {
-		getActiveWorkbenchWindow().getShell().forceActive();
-	}
 }
