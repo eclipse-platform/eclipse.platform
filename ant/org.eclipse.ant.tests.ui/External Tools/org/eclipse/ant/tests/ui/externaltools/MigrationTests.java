f53eede1ee2dd8dbdecd6f7a62f396eb853d2c92
Heiko Klare <heiko.klare@vector.com>
2025-12-31 16:25:56 +0100

    Migrate external tool tests to JUnit 5
    - Convert AbstractExternalToolTest into ExternalToolTestUtil class
    - Use JUnit 5 test annotations
    - Migrate assertions to JUnit 5

--- ant/org.eclipse.ant.tests.ui/External Tools/org/eclipse/ant/tests/ui/externaltools/MigrationTests.java
+++ ant/org.eclipse.ant.tests.ui/External Tools/org/eclipse/ant/tests/ui/externaltools/MigrationTests.java
@@ -14,13 +14,18 @@
 package org.eclipse.ant.tests.ui.externaltools;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.eclipse.ant.tests.ui.externaltools.ExternalToolTestUtil.get20AntArgumentMap;
+import static org.eclipse.ant.tests.ui.externaltools.ExternalToolTestUtil.get20ProgramArgumentMap;
+import static org.eclipse.ant.tests.ui.externaltools.ExternalToolTestUtil.get21AntArgumentMap;
+import static org.eclipse.ant.tests.ui.externaltools.ExternalToolTestUtil.get21ProgramArgumentMap;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.Map;
 
 import org.eclipse.ant.internal.launching.AntLaunchingUtil;
 import org.eclipse.ant.launching.IAntLaunchConstants;
+import org.eclipse.ant.tests.ui.testplugin.AntUITest;
 import org.eclipse.core.externaltools.internal.IExternalToolConstants;
 import org.eclipse.core.externaltools.internal.registry.ExternalToolMigration;
 import org.eclipse.core.runtime.CoreException;
@@ -28,13 +33,14 @@ import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.debug.ui.RefreshTab;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Tests migration of Ant and External Tool configurations from old formats to the current format.
  */
 @SuppressWarnings("restriction")
-public class MigrationTests extends AbstractExternalToolTest {
+@AntUITest
+public class MigrationTests {
 
 	/**
 	 * Tests migration of arguments from an Eclipse 2.0 Ant buildfile configuration to a current launch configuration.
@@ -43,14 +49,14 @@ public class MigrationTests extends AbstractExternalToolTest {
 	public void test20AntMigration() throws CoreException {
 		Map<String, String> argumentMap = get20AntArgumentMap();
 		ILaunchConfigurationWorkingCopy config = ExternalToolMigration.configFromArgumentMap(argumentMap);
-		assertNotNull("Migration failed", config); //$NON-NLS-1$
+		assertNotNull(config, "Migration failed"); //$NON-NLS-1$
 
-		assertEquals("Wrong configuration type", IAntLaunchConstants.ID_ANT_BUILDER_LAUNCH_CONFIGURATION_TYPE, config.getType().getIdentifier()); //$NON-NLS-1$
+		assertEquals(IAntLaunchConstants.ID_ANT_BUILDER_LAUNCH_CONFIGURATION_TYPE, config.getType().getIdentifier(), "Wrong configuration type"); //$NON-NLS-1$
 		assertEquals("ant tool", config.getName()); //$NON-NLS-1$
 		assertEquals("location", config.getAttribute(IExternalToolConstants.ATTR_LOCATION, "")); //$NON-NLS-1$ //$NON-NLS-2$
 		assertEquals("refresh scope", config.getAttribute(RefreshTab.ATTR_REFRESH_SCOPE, "")); //$NON-NLS-1$ //$NON-NLS-2$
 		String[] targets = AntLaunchingUtil.getTargetNames(config);
-		assertNotNull("No targets found", targets); //$NON-NLS-1$
+		assertNotNull(targets, "No targets found"); //$NON-NLS-1$
 		assertThat(targets).containsExactly("target1", "target2"); //$NON-NLS-1$ //$NON-NLS-2$
 		assertEquals(true, config.getAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, false));
 		assertEquals(true, config.getAttribute(IDebugUIConstants.ATTR_LAUNCH_IN_BACKGROUND, false));
@@ -66,7 +72,8 @@ public class MigrationTests extends AbstractExternalToolTest {
 	public void test20ProgramMigration() throws CoreException {
 		Map<String, String> argumentMap = get20ProgramArgumentMap();
 		ILaunchConfigurationWorkingCopy config = ExternalToolMigration.configFromArgumentMap(argumentMap);
-		assertEquals("Wrong configuration type", IExternalToolConstants.ID_PROGRAM_BUILDER_LAUNCH_CONFIGURATION_TYPE, config.getType().getIdentifier()); //$NON-NLS-1$
+		assertEquals(IExternalToolConstants.ID_PROGRAM_BUILDER_LAUNCH_CONFIGURATION_TYPE,
+				config.getType().getIdentifier(), "Wrong configuration type"); //$NON-NLS-1$
 		assertEquals("program tool", config.getName()); //$NON-NLS-1$
 		assertEquals("location", config.getAttribute(IExternalToolConstants.ATTR_LOCATION, "")); //$NON-NLS-1$ //$NON-NLS-2$
 		assertEquals("refresh scope", config.getAttribute(RefreshTab.ATTR_REFRESH_SCOPE, "")); //$NON-NLS-1$ //$NON-NLS-2$
@@ -84,9 +91,10 @@ public class MigrationTests extends AbstractExternalToolTest {
 	public void test21AntMigration() throws CoreException {
 		Map<String, String> argumentMap = get21AntArgumentMap();
 		ILaunchConfigurationWorkingCopy config = ExternalToolMigration.configFromArgumentMap(argumentMap);
-		assertNotNull("Migration failed", config); //$NON-NLS-1$
+		assertNotNull(config, "Migration failed"); //$NON-NLS-1$
 
-		assertEquals("Wrong config type", IAntLaunchConstants.ID_ANT_BUILDER_LAUNCH_CONFIGURATION_TYPE, config.getType().getIdentifier()); //$NON-NLS-1$
+		assertEquals(IAntLaunchConstants.ID_ANT_BUILDER_LAUNCH_CONFIGURATION_TYPE, config.getType().getIdentifier(),
+				"Wrong config type"); //$NON-NLS-1$
 		assertEquals("ant config", config.getName()); //$NON-NLS-1$
 		assertEquals("location", config.getAttribute(IExternalToolConstants.ATTR_LOCATION, "")); //$NON-NLS-1$ //$NON-NLS-2$
 		assertEquals("working directory", config.getAttribute(IExternalToolConstants.ATTR_WORKING_DIRECTORY, "")); //$NON-NLS-1$ //$NON-NLS-2$
@@ -109,9 +117,10 @@ public class MigrationTests extends AbstractExternalToolTest {
 	public void test21ProgramMigration() throws CoreException {
 		Map<String, String> argumentMap = get21ProgramArgumentMap();
 		ILaunchConfigurationWorkingCopy config = ExternalToolMigration.configFromArgumentMap(argumentMap);
-		assertNotNull("Migration failed", config); //$NON-NLS-1$
+		assertNotNull(config, "Migration failed"); //$NON-NLS-1$
 
-		assertEquals("Wrong config type", IExternalToolConstants.ID_PROGRAM_BUILDER_LAUNCH_CONFIGURATION_TYPE, config.getType().getIdentifier()); //$NON-NLS-1$
+		assertEquals(IExternalToolConstants.ID_PROGRAM_BUILDER_LAUNCH_CONFIGURATION_TYPE,
+				config.getType().getIdentifier(), "Wrong config type"); //$NON-NLS-1$
 		assertEquals("program config", config.getName()); //$NON-NLS-1$
 		assertEquals("location", config.getAttribute(IExternalToolConstants.ATTR_LOCATION, "")); //$NON-NLS-1$ //$NON-NLS-2$
 		assertEquals("working directory", config.getAttribute(IExternalToolConstants.ATTR_WORKING_DIRECTORY, "")); //$NON-NLS-1$ //$NON-NLS-2$
