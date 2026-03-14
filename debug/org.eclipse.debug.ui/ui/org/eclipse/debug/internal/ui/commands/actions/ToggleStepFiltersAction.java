dc826d7817457203f6ec4c85b7d8662b3e3c276b
sougandhs <sougandh.s@ibm.com>
2026-03-14 07:05:18 +0530

    Initialize Step Filters button state from DebugUITools preference
    see https://github.com/eclipse-jdt/eclipse.jdt.debug/issues/914

--- debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/commands/actions/ToggleStepFiltersAction.java
+++ debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/commands/actions/ToggleStepFiltersAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2013 IBM Corporation and others.
+ * Copyright (c) 2006, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -131,6 +131,7 @@ public class ToggleStepFiltersAction extends DebugCommandAction implements IPref
 		if (node != null) {
 			node.addPreferenceChangeListener(this);
 		}
+		setChecked(DebugUITools.isUseStepFilters());
 	}
 
 	@Override
