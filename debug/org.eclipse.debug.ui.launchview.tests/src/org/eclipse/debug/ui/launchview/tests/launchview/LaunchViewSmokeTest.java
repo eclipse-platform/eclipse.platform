66d71d134855f06fb7b7d91b088895d38c5d0a66
Александър Куртаков <akurtakov@gmail.com>
2026-06-04 13:09:25 +0300

    Fix cleanup for launchview tests
    PDE cleanup removed dependency on org.eclipse.debug.ui.launchview as it
    is needed for showView(viewId) to work.
    Adding @Referenced to LaunchConfigurationViewPlugin in the test to hint
    the tool. An x-friend in org.eclipse.debug.ui.launchview is needed as
    all exported packages are internal.
    Should fix
    https://github.com/eclipse-platform/eclipse.platform/pull/2692 .

--- debug/org.eclipse.debug.ui.launchview.tests/src/org/eclipse/debug/ui/launchview/tests/launchview/LaunchViewSmokeTest.java
+++ debug/org.eclipse.debug.ui.launchview.tests/src/org/eclipse/debug/ui/launchview/tests/launchview/LaunchViewSmokeTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 SSI Schaefer IT Solutions GmbH and others.
+ * Copyright (c) 2021, 2026 SSI Schaefer IT Solutions GmbH and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -14,25 +14,23 @@
 package org.eclipse.debug.ui.launchview.tests.launchview;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
 
+import org.eclipse.debug.ui.launchview.LaunchConfigurationViewPlugin;
 import org.eclipse.debug.ui.launchview.tests.AbstractLaunchViewTest;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.junit.jupiter.api.Test;
+import org.osgi.annotation.bundle.Referenced;
 
+@Referenced(LaunchConfigurationViewPlugin.class)
 public class LaunchViewSmokeTest extends AbstractLaunchViewTest {
 
 	@Test
-	public void testOpenView() {
+	public void testOpenView() throws PartInitException {
 		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
 		assertNotNull(page, "The active workbench page should not be null");
-		try {
-			page.showView("org.eclipse.debug.ui.launchView"); //$NON-NLS-1$
-		} catch (PartInitException exception) {
-			fail("Failed to open launch configuration view");
-		}
+		page.showView("org.eclipse.debug.ui.launchView"); //$NON-NLS-1$
 
 	}
 
