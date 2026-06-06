15d3813848f96954cd24c06102aeb140c873d10c
Sougandh S <sougandh.s@ibm.com>
2026-06-06 06:00:28 +0530

    Create Launch Group from selected launch configurations
    Add a context menu action to create a new Launch Group from the
    currently selected launch configurations. The selected configurations
    are automatically added as group members when the Launch Group is
    created.

--- debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/LaunchConfigurationsDialog.java
+++ debug/org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/LaunchConfigurationsDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2025 IBM Corporation and others.
+ *  Copyright (c) 2000, 2026 IBM Corporation and others.
  *
  *  This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License 2.0
@@ -863,6 +863,16 @@ public class LaunchConfigurationsDialog extends TitleAreaDialog implements ILaun
 		return (AbstractLaunchConfigurationAction)fLaunchConfigurationView.getAction(CreateLaunchConfigurationAction.ID_CREATE_ACTION);
 	}
 
+	/**
+	 * Gets the quick group launch menu action
+	 *
+	 * @return the quick group launch menu action
+	 *
+	 */
+	protected AbstractLaunchConfigurationAction getQuickGroupLaunchAction() {
+		return (AbstractLaunchConfigurationAction) fLaunchConfigurationView
+				.getAction(QuickGroupLaunch.ID_QUICK_GROUP_LAUNCH_ACTION);
+	}
 	/**
 	 * Gets the new prototype menu action
 	 *
